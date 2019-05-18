7dfa53e1b6aa29bd624545057eeb2f21a5fbaa88
Thorsten Zoerner <me@thorsten-zoerner.com>
2019-05-18 15:32:08 +0200

    added Clearing

--- dist/correntlyElements.js
+++ dist/correntlyElements.js
@@ -336,6 +336,65 @@
       refreshReading();
       setInterval(refreshReading,60000);
     },
+    $.fn.correntlyClearing=function(account) {
+      let q = account;
+      let zip = "10117";
+      let idx = 0;
+
+      if(this.attr("data-account") != null ) q=this.attr("data-account");
+      if(this.attr("account") != null ) q=this.attr("account");
+
+      if(this.attr("data-zip") != null ) zip=this.attr("data-zip");
+      if(this.attr("zip") != null ) zip=this.attr("zip");
+
+      if(this.attr("data-plz") != null ) zip=this.attr("data-plz");
+      if(this.attr("plz") != null ) zip=this.attr("plz");
+
+      if(this.attr("data-idx") != null ) idx=this.attr("data-idx");
+      if(this.attr("idx") != null ) idx=this.attr("idx");
+
+      if(q == null) q = $.getUrlVar('a');
+      if(q == null) q="0x26D53FC47581E8F9f94Eadf71eFd7F57C931b9D9";
+      const tokWh = function(n) {
+        return (n/1000).toFixed(3).replace('.',',');
+      }
+      const parent = this;
+      parent.html("<span class='text-muted'>wird geladen...</span>");
+      $.getJSON("https://api.corrently.io/core/tarif?&zip="+zip,function(tarifinfo) {
+          let tarif = tarifinfo[idx*1];
+          const refreshReading = function() {
+              $.getJSON("https://api.corrently.io/core/reading?&history=3600000&account="+q,function(data) {
+                let html = "<div class='table-responsive'><table class='table table-sm'>";
+
+                html+="<tr><th>Belieferungsbeginn</th><td style='text-align:right'>"+new Date(data.firstReading.timeStamp).toLocaleString()+"</td><td>("+((data.timeStamp-data.firstReading.timeStamp)/86400000).toFixed(1).replace('.',',')+" Tage)</td></tr>";
+                html+="<tr><td>&nbsp;Zählerstand</td><td style='text-align:right'>"+tokWh(data.firstReading["1.8.0"])+"</td><td>&nbsp;</td><td class='text-muted'>kWh</td></tr>";
+                html+="<tr><td>&nbsp;Grünstrom</td><td style='text-align:right'>"+tokWh(data.firstReading["1.8.1"])+"</td><td>&nbsp;</td><td class='text-muted'>kWh</td></tr>";
+                html+="<tr><td>&nbsp;Graustrom</td><td style='text-align:right'>"+tokWh(data.firstReading["1.8.2"])+"</td><td>&nbsp;</td><td class='text-muted'>kWh</td></tr>";
+                html+="<tr><th>Aktuell</th><td style='text-align:right'>"+new Date(data.timeStamp).toLocaleString()+"</td><td>&nbsp;</td><td>&nbsp;</td></tr>";
+                html+="<tr><td>&nbsp;Zählerstand</td><td style='text-align:right'>"+tokWh(data["1.8.0"])+"</td><td>("+tokWh(data["1.8.0"]-data.firstReading["1.8.0"])+")</td><td class='text-muted'>kWh</td></tr>";
+                html+="<tr><td>&nbsp;Grünstrom</td><td style='text-align:right'>"+tokWh(data["1.8.1"])+"</td><td>("+tokWh(data["1.8.1"]-data.firstReading["1.8.1"])+")</td><td class='text-muted'>kWh</td></tr>";
+                html+="<tr><td>&nbsp;Graustrom</td><td style='text-align:right'>"+tokWh(data["1.8.2"])+"</td><td>("+tokWh(data["1.8.2"]-data.firstReading["1.8.2"])+")</td><td class='text-muted'>kWh</td></tr>";
+                html+="<tr><td colspan='3'>&nbsp;</td></tr>";
+                html+="<tr><th>Kosten</th><td style='text-align:right'>"+new Date(data.timeStamp).toLocaleString()+"</td></tr>";
+                let gp = (((tarif.gp*12)/365)*((data.timeStamp-data.firstReading.timeStamp)/86400000));
+                let ap = ((tarif.ap/100000)*(data["1.8.0"]-data.firstReading["1.8.0"]));
+                let bp = ((2/100000)*(data["1.8.1"]-data.firstReading["1.8.1"]));
+                html+="<tr><td>&nbsp;Grundgebühr</td><td style='text-align:right'>"+gp.toFixed(4).replace('.',',')+"</td><td>&nbsp;</td><td class='text-muted'>EUR</td></tr>";
+                html+="<tr><td>&nbsp;Energiekosten</td><td style='text-align:right'>"+ap.toFixed(4).replace('.',',')+"</td><td>&nbsp;</td><td class='text-muted'>EUR</td></tr>";
+                html+="<tr><td>Bezugskosten</td><td style='text-align:right'>"+(gp+ap).toFixed(4).replace('.',',')+"</td><td>&nbsp;</td><td class='text-muted'>EUR</td></tr>";
+                html+="<tr><td>&nbsp;- Bonus</td><td style='text-align:right'>"+bp.toFixed(4).replace('.',',')+"</td><td>&nbsp;</td><td class='text-muted'>EUR</td></tr>";
+                html+="<tr><th>Brutto Kosten</th><td style='text-align:right'>"+((gp+ap)-bp).toFixed(4).replace('.',',')+"</td><td>&nbsp;</td><td class='text-muted'>EUR</td></tr>";
+                html+= "</table></div>";
+                parent.html(html);
+                if(parent == null) {
+                  $('#gsi_card').html(html);
+                }
+              });
+          };
+          refreshReading();
+          setInterval(refreshReading,60000);
+      });
+    },
     /**
     *  Ausgabe der Zählerstände eines Corrently Account als MSCONS Nachricht (in einem Code Block)
     */
@@ -379,6 +438,40 @@
       refreshReading();
       setInterval(refreshReading,60000);
     }
+}( jQuery ));
+
+(function ( $ ) {
+  /**
+   * Ausgabe der Zählerstände eines Corrently Account als  HTML Tabelle
+   */
+    $.fn.correntlyTarif=function(zip) {
+      let parent = this;
+      let q = zip;
+      if(q == null) q = $.getUrlVar('p');
+      if(this.attr("data-zip") != null ) q=this.attr("data-zip");
+      if(this.attr("zip") != null ) q=this.attr("zip");
+      if(this.attr("data-plz") != null ) q=this.attr("data-plz");
+      if(this.attr("plz") != null ) q=this.attr("plz");
+
+      $.getJSON("https://api.corrently.io/core/tarif?&zip="+q,function(data) {
+            let html = "<table class='table table-condensed'>";
+            html+="<tr><th>Ortsteil</th><th>Arbeitspreis (je kWh)</th><th>Grundpreis (je Monat)</th><th>&nbsp;</th></tr>";
+            for(let i=0;i<data.length;i++) {
+              if(data[i].subcity.length > 0) {
+              html+="<tr><td>"+data[i].subcity+"</td>";
+            } else {
+              html+="<tr><td>"+q+"</td>";
+            }
+              html+="<td>"+(data[i].ap/100).toFixed(4).replace('.',',')+"</td>";
+              html+="<td>"+(data[i].gp).replace('.',',')+"</td>";
+              html+="<td><a href='https://corrently.energy/stromprodukte/"+q+"/' class='btn btn-sm btn-warning'>Details</td>";
+              html+="</tr>";
+            }
+            html+="</table>";
+            parent.html(html);
+      });
+    }
+
 }( jQuery ));
 
 $.extend({
