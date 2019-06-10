a356ba630ae42b6c8c7ac67ea1d310b21d8e3aeb
Thorsten Zoerner <me@thorsten-zoerner.com>
2019-06-10 17:45:13 +0200

    Auto

--- dist/correntlyElements.js
+++ dist/correntlyElements.js
@@ -269,11 +269,11 @@
         console.log(sum_from,sum_to);
         if(sum_from>sum_to) {
           if((sum_to+sum_from)>0) {
-            html+="Es wird "+(((sum_to/(sum_from+sum_to))*100)).toFixed(1).replace('.',',')+"% mehr Strom aus anderen Orten bezogen, als in andere Orte geliefert.";
+            html+="Es wird "+(((sum_to/(sum_from+sum_to))*100)+100).toFixed(1).replace('.',',')+"% mehr Strom aus anderen Orten bezogen, als in andere Orte geliefert.";
             }
         } else {
           if((sum_to+sum_from)>0) {
-            html+="Es wird "+(((sum_from/(sum_from+sum_to))*100)).toFixed(1).replace('.',',')+"% mehr Strom an anderen Orten geliefert, als aus anderen Orte bezogent.";
+            html+="Es wird "+(((sum_from/(sum_from+sum_to))*100)+100).toFixed(1).replace('.',',')+"% mehr Strom an anderen Orten geliefert, als aus anderen Orte bezogent.";
           }
         }
         html+="</p>";
