d6ea63edf0cc0a3b7198dc91b946527ba553f07c
Thorsten Zoerner <me@thorsten-zoerner.com>
2019-10-09 10:45:53 +0200

    Auto

--- dist/correntlyElements.js
+++ dist/correntlyElements.js
@@ -369,6 +369,32 @@
     }
 }( jQuery ));
 
+(function ( $ ) {
+  $.fn.correntlyIoT=function(account) {
+    let q = account;
+    if(this.attr("data-account") != null ) q=this.attr("data-account");
+    if(this.attr("account") != null ) q=this.attr("account");
+
+    if(q == null) {
+      if((typeof  $.getUrlVar('a') != "undefined ")&&(q == null)) q = $.getUrlVar('a');
+      if((window.localStorage.getItem("ce_account") != null)&&(q == null)) q = window.localStorage.getItem("ce_account");
+      if(q == null) q="0x036b01A93E6a920aeE77f577C60Dec8E7D22Bf5d";
+    }
+    let html  = "-";
+    parent.html(html);
+
+    const refreshReading = function() {
+        $.getJSON("https://api.corrently.io/core/iot?account="+q,function(data) {
+            parent.html(data.result.value);
+            parent.attr('title',new Date(data.result.timeStamp).toLocaleString());
+        });
+    }
+
+    refreshReading();
+    setInterval(refreshReading,60000);
+  }
+}( jQuery ));
+
 $.extend({
   correntlyQLogout:function() {
     window.localStorage.removeItem("ce_qcode");
